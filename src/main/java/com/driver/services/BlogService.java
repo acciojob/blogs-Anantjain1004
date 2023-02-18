2b7d0b17f0e11db3730ef15dbf2ae2cbe5792fd6
Anantjain1004 <jainanant895@gmail.com>
2023-02-18 17:44:47 +0530

    5

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -54,50 +54,6 @@
 //    }
 //}
 
-//package com.driver.services;
-//
-//import com.driver.models.Blog;
-//import com.driver.models.Image;
-//import com.driver.models.User;
-//import com.driver.repositories.BlogRepository;
-//import com.driver.repositories.ImageRepository;
-//import com.driver.repositories.UserRepository;
-//import com.sun.istack.NotNull;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.ArrayList;
-//import java.util.Date;
-//import java.util.List;
-//
-//@Service
-//public class BlogService {
-//    @Autowired
-//    BlogRepository blogRepository1;
-//
-//    @Autowired
-//    UserRepository userRepository1;
-//
-//    public Blog createAndReturnBlog(Integer userId, String title, String content) {
-//        //create a blog at the current time
-//
-////        if(!userRepository1.findById(userId).isPresent()){
-////            throw new Exception();
-////        }
-//        User user = userRepository1.findById(userId).get();
-//        Blog blog = new Blog(user,title,content);
-//        userRepository1.save(user); //Blog saved in repo by cascading
-//        user.getBlogList().add(blog);
-//        return blog;
-//
-//    }
-//
-//    public void deleteBlog(int blogId){
-//        //delete blog and corresponding images
-//        blogRepository1.deleteById(blogId);
-//    }
-//}
-
 package com.driver.services;
 
 import com.driver.models.Blog;
@@ -122,12 +78,12 @@ public class BlogService {
     @Autowired
     UserRepository userRepository1;
 
-    public Blog createAndReturnBlog(Integer userId, String title, String content) throws Exception {
+    public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
 
-        if(!userRepository1.findById(userId).isPresent()){
-            throw new Exception();
-        }
+//        if(!userRepository1.findById(userId).isPresent()){
+//            throw new Exception();
+//        }
         User user = userRepository1.findById(userId).get();
         Blog blog = new Blog(user,title,content);
         userRepository1.save(user); //Blog saved in repo by cascading
@@ -141,3 +97,47 @@ public class BlogService {
         blogRepository1.deleteById(blogId);
     }
 }
+
+//package com.driver.services;
+//
+//import com.driver.models.Blog;
+//import com.driver.models.Image;
+//import com.driver.models.User;
+//import com.driver.repositories.BlogRepository;
+//import com.driver.repositories.ImageRepository;
+//import com.driver.repositories.UserRepository;
+//import com.sun.istack.NotNull;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.Date;
+//import java.util.List;
+//
+//@Service
+//public class BlogService {
+//    @Autowired
+//    BlogRepository blogRepository1;
+//
+//    @Autowired
+//    UserRepository userRepository1;
+//
+//    public Blog createAndReturnBlog(Integer userId, String title, String content) throws Exception {
+//        //create a blog at the current time
+//
+//        if(!userRepository1.findById(userId).isPresent()){
+//            throw new Exception();
+//        }
+//        User user = userRepository1.findById(userId).get();
+//        Blog blog = new Blog(user,title,content);
+//        userRepository1.save(user); //Blog saved in repo by cascading
+//        user.getBlogList().add(blog);
+//        return blog;
+//
+//    }
+//
+//    public void deleteBlog(int blogId){
+//        //delete blog and corresponding images
+//        blogRepository1.deleteById(blogId);
+//    }
+//}
